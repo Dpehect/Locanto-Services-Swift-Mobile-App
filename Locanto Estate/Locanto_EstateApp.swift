e6755a820e935e290198e6ffe658ef568f6f5476
Yunus Emre Gürlek <105203804+Dpehect@users.noreply.github.com>
2026-06-29 23:53:49 +0300

    Initial Commit

---
+++ Locanto Estate/Locanto_EstateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Locanto_EstateApp.swift
+//  Locanto Estate
+//
+//  Created by Yunus Emre Gürlek on 29.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Locanto_EstateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Locanto EstateTests/Locanto_EstateTests.swift
@@ -0,0 +1,18 @@
+//
+//  Locanto_EstateTests.swift
+//  Locanto EstateTests
+//
+//  Created by Yunus Emre Gürlek on 29.06.2026.
+//
+
+import Testing
+
+struct Locanto_EstateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
